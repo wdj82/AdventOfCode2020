8533685becd540f05f6266a8551da3aa07f2603b
William Jones <kc5uyn@gmail.com>
2020-12-01 13:21:30 -0600

    cleaned up

--- day 1/dayone.js
+++ day 1/dayone.js
@@ -5,10 +5,9 @@ const inputArray = input.split('\n').map(Number);
 function firstAnswer() {
     for (let i = 0; i < inputArray.length; i++) {
         const num1 = inputArray[i];
-        const target = 2020 - num1;
         for (let j = i + 1; j < inputArray.length; j++) {
             const num2 = inputArray[j];
-            if (num2 === target) {
+            if (num1 + num2 === 2020) {
                 return num1 * num2;
             }
         }
@@ -20,10 +19,9 @@ function secondAnswer() {
         const num1 = inputArray[i];
         for (let j = i + 1; j < inputArray.length; j++) {
             const num2 = inputArray[j];
-            const target = 2020 - num1 - num2;
             for (let k = j + 1; k < inputArray.length; k++) {
                 const num3 = inputArray[k];
-                if (num3 === target) {
+                if (num1 + num2 + num3 === 2020) {
                     return num1 * num2 * num3;
                 }
             }
